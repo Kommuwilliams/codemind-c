2b2fa26ea403e116ea1c1af1b830a6795f05746f
Kommuwilliams <119730449+Kommuwilliams@users.noreply.github.com>
2023-05-23 11:07:46 +0530

    #include<stdio.h> int main() {  int n1=0,n2=1,n3,i,num;
     scanf("%d",&num);    
     printf("0 1");
    
     for(i=2;i<num;++i)    
     {    
      n3=n1+n2;    
      printf(" %d",n3);    
      n1=n2;    
      n2=n3;    
     }  
      return 0;  
     }

--- fibonacci.c
+++ fibonacci.c
@@ -1,13 +1,18 @@
-#include<stdio.h>
-int main()
-{
-    int a,i,b=0,c,n=1;
-    scanf("%d",&a);
-    for(i=0;i<a;i++)
-    {
-        printf("%d ",b);
-        c=b+n;
-        b=n;
-        n=c;
-    }
-}
+#include<stdio.h>    
+int main()    
+{    
+ int n1=0,n2=1,n3,i,num;    
+   
+ scanf("%d",&num);    
+ printf("0 1");
+
+ for(i=2;i<num;++i)    
+ {    
+  n3=n1+n2;    
+  printf(" %d",n3);    
+  n1=n2;    
+  n2=n3;    
+ }  
+  return 0;  
+ } 
+
